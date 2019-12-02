23deb61b186135c0bdc1291786e5eb339a0101c7
sax <sax@T440>
2019-12-02 06:10:39 +0000

    tidy up formatting a bit

--- 02b.py
+++ 02b.py
@@ -1,7 +1,9 @@
 import sys
+
 add = 1
 multiply = 2
 halt = 99
+
 
 def main():
 
@@ -18,39 +20,41 @@ def main():
             memory[2] = verb
             if run_program(memory[:]) == target_value:
                 print(100 * noun + verb)
-                
-    
+
 
 def run_program(memory):
     pointer = 0
     finished = False
 
-
     while finished == False:
         opcode = memory[pointer]
         if opcode == add:
-            memory[memory[pointer + 3]] = memory[memory[pointer+2]] + memory[memory[pointer + 1]]
+            memory[memory[pointer + 3]] = (
+                memory[memory[pointer + 2]] + memory[memory[pointer + 1]]
+            )
             pointer += 4
         elif opcode == multiply:
-            memory[memory[pointer + 3]] = memory[memory[pointer+2 ]]* memory[memory[pointer + 1]]
+            memory[memory[pointer + 3]] = (
+                memory[memory[pointer + 2]] * memory[memory[pointer + 1]]
+            )
             pointer += 4
         elif opcode == halt:
             finished = True
         else:
-            raise ValueError(f"Unrecognised opcode {memory[pointer]} at position {pointer}.\n Program:\n{memory}")
+            raise ValueError(
+                f"Unrecognised opcode {memory[pointer]} at position {pointer}.\n Program:\n{memory}"
+            )
 
     # for i in range(len(memory)):
     #     print(memory[i],end=',')
     # print()
     return memory[0]
 
-    
-
 
 def get_memory(filename):
     memory = []
     with open(filename) as f:
-        for value in f.read().strip().split(','):
+        for value in f.read().strip().split(","):
             memory.append(int(value))
     return memory
 
