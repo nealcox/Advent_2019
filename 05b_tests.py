df6b2d50eed1a8e11d6ecfddff0acb59b97f495d
sax <sax@T440>
2019-12-05 23:28:14 +0000

    Tidy up and make intcode_machine.py be a separate class

--- 05b_tests.py
+++ 05b_tests.py
@@ -1,58 +1,59 @@
 import unittest
 
-from day05b import run_program
+from intcode_machine import IntcodeMachine
 
 class RunProgramTest(unittest.TestCase):
     def test_equals_8_position(self):
         # Using position mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
-        self.assertEqual(run_program([3,9,8,9,10,9,4,9,99,-1,8],[1]),0)
-        self.assertEqual(run_program([3,9,8,9,10,9,4,9,99,-1,8],[7]),0)
-        self.assertEqual(run_program([3,9,8,9,10,9,4,9,99,-1,8],[8]),1)
-        self.assertEqual(run_program([3,9,8,9,10,9,4,9,99,-1,8],[9]),0)
+        self.assertEqual(IntcodeMachine([3,9,8,9,10,9,4,9,99,-1,8]).run_program([1]),0)
+        self.assertEqual(IntcodeMachine([3,9,8,9,10,9,4,9,99,-1,8]).run_program([7]),0)
+        self.assertEqual(IntcodeMachine([3,9,8,9,10,9,4,9,99,-1,8]).run_program([8]),1)
+        self.assertEqual(IntcodeMachine([3,9,8,9,10,9,4,9,99,-1,8]).run_program([9]),0)
+
     def test_less_than_8_position(self):
         # Using position mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
-        self.assertEqual(run_program([3,9,7,9,10,9,4,9,99,-1,8],[1]),1)
-        self.assertEqual(run_program([3,9,7,9,10,9,4,9,99,-1,8],[7]),1)
-        self.assertEqual(run_program([3,9,7,9,10,9,4,9,99,-1,8],[8]),0)
-        self.assertEqual(run_program([3,9,7,9,10,9,4,9,99,-1,8],[9]),0)
+        self.assertEqual(IntcodeMachine([3,9,7,9,10,9,4,9,99,-1,8]).run_program([1]),1)
+        self.assertEqual(IntcodeMachine([3,9,7,9,10,9,4,9,99,-1,8]).run_program([7]),1)
+        self.assertEqual(IntcodeMachine([3,9,7,9,10,9,4,9,99,-1,8]).run_program([8]),0)
+        self.assertEqual(IntcodeMachine([3,9,7,9,10,9,4,9,99,-1,8]).run_program([9]),0)
     def test_equals_8_immediate(self):
         # Using immediate mode, consider whether the input is equal to 8; output 1 (if it is) or 0 (if it is not).
-        self.assertEqual(run_program([3,3,1108,-1,8,3,4,3,99],[1]),0)
-        self.assertEqual(run_program([3,3,1108,-1,8,3,4,3,99],[7]),0)
-        self.assertEqual(run_program([3,3,1108,-1,8,3,4,3,99],[8]),1)
-        self.assertEqual(run_program([3,3,1108,-1,8,3,4,3,99],[9]),0)
+        self.assertEqual(IntcodeMachine([3,3,1108,-1,8,3,4,3,99]).run_program([1]),0)
+        self.assertEqual(IntcodeMachine([3,3,1108,-1,8,3,4,3,99]).run_program([7]),0)
+        self.assertEqual(IntcodeMachine([3,3,1108,-1,8,3,4,3,99]).run_program([8]),1)
+        self.assertEqual(IntcodeMachine([3,3,1108,-1,8,3,4,3,99]).run_program([9]),0)
     def test_less_than_8_immediate(self):
         # Using immediate mode, consider whether the input is less than 8; output 1 (if it is) or 0 (if it is not).
-        self.assertEqual(run_program([3,3,1107,-1,8,3,4,3,99],[1]),1)
-        self.assertEqual(run_program([3,3,1107,-1,8,3,4,3,99],[7]),1)
-        self.assertEqual(run_program([3,3,1107,-1,8,3,4,3,99],[8]),0)
-        self.assertEqual(run_program([3,3,1107,-1,8,3,4,3,99],[9]),0)
+        self.assertEqual(IntcodeMachine([3,3,1107,-1,8,3,4,3,99]).run_program([1]),1)
+        self.assertEqual(IntcodeMachine([3,3,1107,-1,8,3,4,3,99]).run_program([7]),1)
+        self.assertEqual(IntcodeMachine([3,3,1107,-1,8,3,4,3,99]).run_program([8]),0)
+        self.assertEqual(IntcodeMachine([3,3,1107,-1,8,3,4,3,99]).run_program([9]),0)
     def test_is_input_zero_position(self):
         # jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero Position mode
         program = [3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9]
-        self.assertEqual(run_program(program[:],[1]),1)
-        self.assertEqual(run_program(program[:],[0]),0)
-        self.assertEqual(run_program(program[:],[-1]),1)
+        self.assertEqual(IntcodeMachine(program).run_program([1]),1)
+        self.assertEqual(IntcodeMachine(program).run_program([0]),0)
+        self.assertEqual(IntcodeMachine(program).run_program([-1]),1)
         
     def test_is_input_zero_immediate(self):
         # jump tests that take an input, then output 0 if the input was zero or 1 if the input was non-zero Immediate mode
         program = [3,3,1105,-1,9,1101,0,0,12,4,12,99,1]
-        self.assertEqual(run_program(program[:],[1]),1)
-        self.assertEqual(run_program(program[:],[0]),0)
-        self.assertEqual(run_program(program[:],[-1]),1)
+        self.assertEqual(IntcodeMachine(program).run_program([1]),1)
+        self.assertEqual(IntcodeMachine(program).run_program([0]),0)
+        self.assertEqual(IntcodeMachine(program).run_program([-1]),1)
 
     def test_compare_to_8(self):
         program = [3,21,1008,21,8,20,1005,20,22,107,8,21,20,1006,20,31,
-1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
-999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
-        self.assertEqual(run_program(program[:],[7]),999)
-        self.assertEqual(run_program(program[:],[8]),1000)
-        self.assertEqual(run_program(program[:],[9]),1001)
+                    1106,0,36,98,0,0,1002,21,125,20,4,20,1105,1,46,104,
+                    999,1105,1,46,1101,1000,1,20,4,20,1105,1,46,98,99]
+        self.assertEqual(IntcodeMachine(program).run_program([7]),999)
+        self.assertEqual(IntcodeMachine(program).run_program([8]),1000)
+        self.assertEqual(IntcodeMachine(program).run_program([9]),1001)
 
     def test_jump_if_true(self):
         # jumps
         program = [1105,1,3,1105,0,0,104,1,99]
-        self.assertEqual(run_program(program,[9999]),1)
+        self.assertEqual(IntcodeMachine(program).run_program([9999]),1)
 
 if __name__ == "__main__":
     unittest.main()
---
+++ intcode_machine.py
@@ -0,0 +1,144 @@
+import sys
+
+class IntcodeMachine():
+    def __init__(self,memory=None):
+        self.memory = memory
+        self.memory_ = memory
+
+    def load_memory(self,filename):
+        memory = []
+        with open(filename) as f:
+            for value in f.read().strip().split(","):
+                memory.append(int(value))
+        self.memory = memory
+        self.memory_ = memory[:]
+
+    def reload(self):
+        self.memory = self.memory_[:]
+
+    def run_program(self,inputs = None):
+        memory = self.memory[:]
+        diagnotsic_code = None
+        position_mode = 0
+        immediate_mode = 1
+
+        add = 1
+        mult = 2
+        read = 3
+        out = 4
+        jump_if_true = 5
+        jump_if_false = 6
+        less_than = 7
+        equals = 8
+        halt = 99
+
+        # modal args only
+        args_per_opcode = {
+            add: 2,
+            mult: 2,
+            read: 0,
+            out: 1,
+            halt: 0,
+            jump_if_true: 2,
+            jump_if_false: 2,
+            less_than: 2,
+            equals: 2,
+        }
+
+        pointer = 0
+        finished = False
+
+        while not finished:
+            instruction = memory[pointer]
+            opcode = instruction % 100
+            mode1 = instruction // 100 % 10
+            mode2 = instruction // 1000 % 10
+            mode3 = instruction // 10000 % 10
+
+            if args_per_opcode[opcode] > 0:
+                if mode1 == position_mode:
+                    arg1 = memory[memory[pointer + 1]]
+                elif mode1 == immediate_mode:
+                    arg1 = memory[pointer + 1]
+                else:
+                    raise ValueError(f"Invalid mode for arg 1 in {instruction}")
+                if args_per_opcode[opcode] > 1:
+                    if mode2 == position_mode:
+                        arg2 = memory[memory[pointer + 2]]
+                    elif mode2 == immediate_mode:
+                        arg2 = memory[pointer + 2]
+                    else:
+                        raise ValueError(f"Invalid mode for arg 2 in {instruction}")
+                    if args_per_opcode[opcode] > 2:
+                        if mode3 == position_mode:
+                            arg3 = memory[memory[pointer + 3]]
+                        elif mode3 == immediate_mode:
+                            arg3 = memory[pointer + 3]
+                        else:
+                            raise ValueError(f"Invalid mode for arg 3 in {instruction}")
+
+            if opcode == add:
+                memory[memory[pointer + 3]] = arg1 + arg2
+                pointer += 4
+            elif opcode == mult:
+                memory[memory[pointer + 3]] = arg1 * arg2
+                pointer += 4
+            elif opcode == read:
+                memory[memory[pointer + 1]] = inputs[0]
+                inputs = inputs[1:]
+                pointer += 2
+            elif opcode == out:
+                diagnotsic_code = arg1
+                print(arg1)
+                pointer += 2
+            elif opcode == jump_if_true:
+                if arg1 != 0:
+                    pointer = arg2
+                else:
+                    pointer += 3
+            elif opcode == jump_if_false:
+                if arg1 == 0:
+                    pointer = arg2
+                else:
+                    pointer += 3
+            elif opcode == less_than:
+                if arg1 < arg2:
+                    memory[memory[pointer + 3]] = 1
+                else:
+                    memory[memory[pointer + 3]] = 0
+                pointer += 4
+            elif opcode == equals:
+                if arg1 == arg2:
+                    memory[memory[pointer + 3]] = 1
+                else:
+                    memory[memory[pointer + 3]] = 0
+                pointer += 4
+            elif opcode == halt:
+                finished = True
+            else:
+                raise ValueError(
+                    f"Unrecognised opcode {memory[pointer]} at position {pointer}.\n Program:\n{memory}"
+                )
+            # print(f"instruction pointer: {pointer}")
+
+        return diagnotsic_code
+
+
+        
+
+def main():
+
+    if len(sys.argv) > 1:
+        filename = sys.argv[1]
+    else:
+        filename = "05-input.txt"
+    machine = IntcodeMachine()
+    machine.load_memory(filename)
+
+    inputs = [5]
+
+    print(machine.run_program(inputs))
+
+
+if __name__ == "__main__":
+    main()
